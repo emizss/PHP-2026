1d84059fb1ce5468a227b5b1c2afe367237bc91c
emilli <emillisant00oo@gmail.com>
2026-08-05 14:38:38 -0300

    criei o arquivo de conexao com o banco

---
+++ conexaoDB.php
@@ -0,0 +1,19 @@
+<?php
+
+function getConexao(){
+$dsn = "pgsql:host=localhost;port=5432;dbname=floricultura;";
+$usuario = "postgres"; 
+$senha = "123";
+
+
+
+try {
+    $pdo = new PDO($dsn, $usuario, $senha);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $pdo;
+    echo "Conectado ao PostgreSQL com sucesso!";
+} catch (PDOException $e) {
+    echo "Erro ao conectar ao Postgres: " . $e->getMessage();
+}
+
+}
