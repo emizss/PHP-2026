8e39add2a850f90053489ca2b77340acf8e30ca7
Emilli <emillisant00oo@gmail.com>
2026-08-14 10:34:08 -0300

    Fiz RUD

--- floricultura/classes/Cliente.php
+++ floricultura/classes/Cliente.php
@@ -39,4 +39,41 @@ public function inserir(){
   return $stmt->execute();
 }
 
+// Busca por ID
+public static function buscarPorId(int $id) {
+  $conn = getConexao();
+  $stmt = $conn->prepare("SELECT * FROM cliente WHERE id = :id");
+  $stmt->bindParam(":id", $id);
+  $stmt->execute();
+  return $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+// Busca todos
+public static function listarTodos() {
+  $conn = getConexao();
+  $stmt = $conn->query("SELECT * FROM cliente");
+  return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+// Metódo UPDATE
+public function atualizar(int $id) {
+  $conn = getConexao();
+  $stmt = $conn->prepare("UPDATE cliente SET nome = :nome, cpf = :cpf, telefone =: celular, data_nasc =: dataNasc WHERE id = :id");
+  $stmt->bindParam(":id", $id);
+  $stmt->bindParam(":nome", $this->nome);
+  $stmt->bindParam(":cpf", $this->cpf);
+  $stmt->bindParam(":celular", $this->celular);
+  $stmt->bindParam(":dataNasc", $this->dataNasc);
+  return $stmt->execute();
+}
+
+// Metódo DELETE
+public static function excluir($id) {
+  $conn = getConexao();
+  $stmt = $conn->prepare("DELETE FROM cliente WHERE id = :id");
+  $stmt->bindParam(":id", $id);
+  return $stmt->execute();
+}
+
+  
 ?>
