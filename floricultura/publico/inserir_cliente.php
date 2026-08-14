e2ba8e7df6d0818fe3a507726c2656501fd50603
Emilli <emillisant00oo@gmail.com>
2026-08-14 12:09:07 -0300

    fiz o formulario de cliente e insert

---
+++ floricultura/publico/inserir_cliente.php
@@ -0,0 +1,19 @@
+<?php
+require_once "../classes/Cliente.php"; 
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $nome     = $_POST['nome'];
+    $cpf      = $_POST['cpf'];
+    $celular  = $_POST['celular'];
+    $dataNasc = $_POST['dataNasc'];
+
+    $cliente = new Cliente($nome, $cpf, $celular, $dataNasc);
+
+    if ($cliente->inserir()) {
+        echo "Cliente cadastrado com sucesso!";
+    } else {
+        echo "Erro ao cadastrar cliente.";
+    }
+}
+?>
+
