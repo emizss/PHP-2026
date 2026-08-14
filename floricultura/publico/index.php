d434ce4a038f709326a81d9bf75358e57853f440
Emilli <emillisant00oo@gmail.com>
2026-08-14 12:08:40 -0300

    criei o index com os caminhos para os forms

---
+++ floricultura/publico/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Floricultura</title>
+</head>
+<body>
+  <h2>MENU</h2>
+  <ul>
+    <li>
+      <a href="funcionario.html"> Inserir funcionário</a>
+    </li>
+    <li>
+      <a href="produto.html"> Inserir produto</a>
+    </li>
+    <li>
+      <a href="cliente.html"> Inserir cliente</a>
+    </li>
+    <li>
+      <a href="venda.html"> Inserir Venda</a>
+    </li>
+  </ul>
+</body>
+</html>
