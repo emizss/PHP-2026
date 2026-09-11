ae0cc156867bab324f3656c7ced41f4f8bc8b176
Emilli <emillisant00oo@gmail.com>
2026-09-11 14:39:45 -0300

    arrumei o index

--- MVC/meu_projeto/index.php
+++ MVC/meu_projeto/index.php
@@ -1,4 +1,25 @@
 <?php
 require_once "config.php";
 
+$modulo = $_GET['modulo'] ?? 'produto';
+$acao =   $_GET['acao']   ??  'listar';
+
+$controllerName = ucfirst($modulo) . "Controller";
+$arquivoController = "controllers/{$controllerName}.php"; ///cudido problema de seguranca
+
+if(file_exists($arquivoController)){
+  require_once $arquivoController;
+  $controller = new $controllerName($db);
+
+  if(method_exists($controller, $acao)){
+    $controller->$acao();
+
+  }else{
+    echo "Ação não encontrada";
+  }
+
+}else{
+  echo "Página não encontrada 404";
+}
+
 ?>
